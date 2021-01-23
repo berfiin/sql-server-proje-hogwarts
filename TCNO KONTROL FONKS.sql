a1b0359b48a1cde871c7c63d19b57f81864130e9
Cumhurcan ÇANKAYA <cumhurcangs97@gmail.com>
2021-01-23 23:53:26 +0300

    Delete TCNO KONTROL FONKS.sql

--- TCNO KONTROL FONKS.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION CHECKING_IDENTITY2 (@Customer_Identity_Number CHAR(11))
-RETURNS TABLE
-AS
-RETURN 
-(SELECT * FROM Customer WHERE Customer_Identity_Number = @Customer_Identity_Number)
-GO
-
-SELECT * FROM CHECKING_IDENTITY2(35364215896)
